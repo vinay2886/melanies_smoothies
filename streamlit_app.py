1b06d5ab69db26f3dc9c49c582278a845a8f151c
vinay2886 <vinay.k.gavara@kipi.bi>
2024-08-09 10:51:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_list:
  # New section to display fruityvice nutrition information
  import requests
  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
- st.text(fruityvice_response)
+ st.text(fruityvice_response.json())
 
 
 time_to_insert = st.button("Submit Order")
