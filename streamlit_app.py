7981dd2a2dbbfde33646b9973e625ab4f7012e3c
vinay2886 <vinay.k.gavara@kipi.bi>
2024-08-09 12:38:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,33 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpaark.function import col
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
 """Choose the fruit you want in your custom Smoothie!"""
 )
-from snowflake.snowpark.functions import col
-name_on_order=st.text_input('Name on Smoothie:')
-st.write('The name on your smoothie will be :',name_on_order)
+name_on_order= st.text_input('Name on Smoothie;')
+st.write('The name on your Smootie will be:',name_on_order)
 
-cnx = st.connectio("snowflake")
-session = cnx.session()
+
+
+
+from snowflake.snowpark.functions import col
+
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    , my_dataframe
-    , max_selections=5
-)
+    'Chose up to 5 ingredients:'
+    ,my_dataframe
+    ,max_selections=5
+    )
+
+
+
 if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
@@ -34,8 +42,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
-
-
-        
-
 
