56239fe4160b11d7b40dfd73c659e9ba21e69c22
vinay2886 <vinay.k.gavara@kipi.bi>
2024-08-16 14:42:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
       fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
       #st.write(ingredients_string)
 
- my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
  
  #st.write(my_insert_stmt)
