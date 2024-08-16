26a48278a20c3437a644041236f1f2ab4a4a50a8
vinay2886 <vinay.k.gavara@kipi.bi>
2024-08-16 13:31:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,15 +41,12 @@ if ingredients_list:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
       fv_df = st.dataframe(data= fruityvice_response.json(),use_container_width=True)
  #st.write(ingredients_string)
+
  my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
  
  #st.write(my_insert_stmt)
  #st.stop()
- # New section to display fruityvice nutrition information   
- import requests
- fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
- st.text(fruityvice_response)
 
 
 time_to_insert = st.button("Submit Order")
